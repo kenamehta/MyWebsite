578d04ead9b9e60c4341ea79c3fae45b6e0a6a5f
kenamehta <kenamehta1995@gmail.com>
2020-08-19 02:59:32 -0700

    Project modal

--- src/components/tabs/About.js
+++ src/components/tabs/About.js
@@ -17,7 +17,7 @@ class About extends Component {
                                 <div style={{ paddingTop: "20px" }} align="left">
                                     <ul>
                                         <li>
-                                            <a href="https://www.sjsu.edu/" target="_blank">San Jose State University</a>
+                                            <a href="https://www.sjsu.edu/" target="_blank" rel="noopener noreferrer">San Jose State University</a>
                                             <div style={{ color: "gray", fontSize: "13px" }}><i>Master's in Software Engineering (Jan 2020 - Expected Dec 2021)</i></div>
                                             <div style={{ fontSize: "13px" }}>Related Courses:
                                                 <ul>
@@ -28,7 +28,7 @@ class About extends Component {
                                             </div>
                                         </li>
                                         <li>
-                                            <a href="http://www.djsce.ac.in/" target="_blank">University of Mumbai</a>
+                                            <a href="http://www.djsce.ac.in/" target="_blank" rel="noopener noreferrer">University of Mumbai</a>
                                             <div style={{ color: "gray", fontSize: "13px" }}><i>Bachelor's in Information Technology (Jun 2013 - Jun 2017)</i></div>
                                             <div style={{ fontSize: "13px" }}>Related Courses:
                                                 <ul>
@@ -50,7 +50,7 @@ class About extends Component {
                                 <div style={{ paddingTop: "20px" }} align="left">
                                     <ul>
                                         <li>
-                                            <a href="https://www.lamresearch.com/" target="_blank">Lam Research</a>
+                                            <a href="https://www.lamresearch.com/" target="_blank" rel="noopener noreferrer">Lam Research</a>
                                             <div style={{ color: "gray", fontSize: "13px" }}><i>Research Assistant (Jun 2020 - Aug 2020)</i></div>
                                             <ul style={{ fontSize: "13px" }}>
                                                 <li>Developed a comprehensive full-stack web application for LAM Research to visualize the log data generated every hour from semi-conductor making machines</li>
@@ -59,7 +59,7 @@ class About extends Component {
                                             </ul>
                                         </li>
                                         <li>
-                                            <a href="https://group.bnpparibas/en/" target="_blank">BNP Paribas - Bank of the West</a>
+                                            <a href="https://group.bnpparibas/en/" target="_blank" rel="noopener noreferrer">BNP Paribas - Bank of the West</a>
                                             <div style={{ color: "gray", fontSize: "13px" }}><i>Software Engineer (Jun 2017 - Jan 2020)</i></div>
                                             <ul style={{ fontSize: "13px" }}>
                                                 <li>Created REST APIs in JAVA SpringBoot MVC framework.</li>
@@ -68,7 +68,7 @@ class About extends Component {
                                             </ul>
                                         </li>
                                         <li>
-                                            <a href="https://www.zaubacorp.com/company/TRIPLETAP-TECHNOLOGIES-PRIVATE-LIMITED/U72900MH2015PTC265102" target="_blank">TripleTap Technologies</a>
+                                            <a href="https://www.zaubacorp.com/company/TRIPLETAP-TECHNOLOGIES-PRIVATE-LIMITED/U72900MH2015PTC265102" target="_blank" rel="noopener noreferrer">TripleTap Technologies</a>
                                             <div style={{ color: "gray", fontSize: "13px" }}><i>Software Engineer Intern(Feb 2016 - Mar 2016)</i></div>
                                             <ul style={{ fontSize: "13px" }}>
                                                 <li>Implemented Google and Facebook OAuth module under the Login and Register module of the website.</li>
@@ -88,13 +88,13 @@ class About extends Component {
                                     <div className="recommendations">
                                         Kena and I worked together for Bank Of The West(BNPP)Treasury IT Team, she was a very creative and supporting Team member, She is Tenacious , intelligent and hugely passionate, She always stood out of one of the most creative minded, She leans into solving the problem not just defining it well, Myself and team enjoyed working with Kena and highly recommend her skills in development I would very much hope to work together with her in future
                                     <div style={{ color: "gray", fontSize: "13px" }}>
-                                            <i><a target="_blank" href="">Sridhar Esampalli</a> - Sr Data Engineer at BNP Paribas (USA)</i>
+                                            <i><a target="_blank" href="https://www.linkedin.com/in/sridhar-esampalli-88087a18/" rel="noopener noreferrer">Sridhar Esampalli</a> - Sr Data Engineer at BNP Paribas (USA)</i>
                                         </div>
                                     </div>
                                     <div className="recommendations mt-5">
                                         Kena worked with me on a project related to Financial Projections. Being a Junior developer few of the things that stood out for her were Ownership, Dedication, Focus and her ability to work efficiently using her prescience. In a short span of time, she developed a strong command over technologies and tools that we used. I highly recommend her
                                     <div style={{ color: "gray", fontSize: "13px" }}>
-                                            <i><a target="_blank" href="">Hardik Gosalia</a> - Technical Lead at BNP Paribas (India)</i>
+                                            <i><a target="_blank" href="https://www.linkedin.com/in/hardik-gosalia-31669a19/" rel="noopener noreferrer">Hardik Gosalia</a> - Technical Lead at BNP Paribas (India)</i>
                                         </div>
                                     </div>
                                 </ul>
@@ -105,13 +105,13 @@ class About extends Component {
                                     <div className="recommendations">
                                         Kena worked under me during the Data Quality Health Check project. She was instrumental in automating the conversion of legacy PLSQL based checks to Informatica based ones. This came as a surprise to me as this was the first project she worked on after joining as a fresher. I greatly endorse her automation skills and her ability to spot inefficiencies in the system
                                     <div style={{ color: "gray", fontSize: "13px" }}>
-                                            <i><a target="_blank" href="">Dhiraj Kadam</a> - Technical Lead at BNP Paribas (India)</i>
+                                            <i><a target="_blank" href="https://www.linkedin.com/in/dhiraj-kadam-b4001438/" rel="noopener noreferrer">Dhiraj Kadam</a> - Technical Lead at BNP Paribas (India)</i>
                                         </div>
                                     </div>
                                     <div className="recommendations mt-5">
                                         I know Kena since the past 1 and half years. She has proved herself to be an outstanding performer at BNP Paribas ISPL. Her dedication, hardwork and ability to grasp on any given module or a business requirement has made her stand out and a go to person. She is really good at understanding client requirements and providing quick solutions. She always accepted new challenges with enthusiasm and committed to the timeline of the project. Kena is a good team player and very supportive to the team members when it comes to knowledge sharing. She will definitely fit into challenging environments
                                     <div style={{ color: "gray", fontSize: "13px" }}>
-                                            <i><a target="_blank" href="">Viral Patel</a> - Sr. Software Engineer at BNP Paribas (India)</i>
+                                            <i><a target="_blank" rel="noopener" href="https://www.linkedin.com/in/viralspatel21/" rel="noopener noreferrer">Viral Patel</a> - Sr. Software Engineer at BNP Paribas (India)</i>
                                         </div>
                                     </div>
                                 </ul>
--- src/components/tabs/Project.js
+++ src/components/tabs/Project.js
@@ -5,8 +5,10 @@ import {
     Grid,
     Card,
     CardContent,
-    CardHeader
+    CardHeader,
+    Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, useMediaQuery
 } from '@material-ui/core/';
+import { useTheme } from '@material-ui/core/styles';
 import {
     Row
 } from 'react-bootstrap';
@@ -17,22 +19,47 @@ const useStyles = theme => ({
     }
 })
 
-class Project extends Component {
-    state = {}
-    render() {
-        const classes = useStyles();
-        return (<div id="project_section" className="project-bg">
-            <div className="" >
-                <h1 className="tabs-heading" style={{ padding: "50px" }}><u>Projects</u></h1>
-           
+export default function Project() {
+    const [open, setOpen] = React.useState(false);
+    const [modalId, setModalId] = React.useState(-1);
+    // const [scroll, setScroll] = React.useState('paper');
+
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClickOpen = (id) => {
+        setModalId(id);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        console.log("hello");
+        setModalId(-1);
+        setOpen(false);
+    };
+    const descriptionElementRef = React.useRef(null);
+    React.useEffect(() => {
+        if (open) {
+            const { current: descriptionElement } = descriptionElementRef;
+            if (descriptionElement !== null) {
+                descriptionElement.focus();
+            }
+        }
+    }, [open]);
+    const classes = useStyles();
+    return (<div id="project_section" className="project-bg">
+        <div className="" >
+            <h1 className="tabs-heading" style={{ padding: "50px" }}><u>Projects</u></h1>
+
             <div className={classes.root + " mt-2 project-inner-bg"}>
                 <Grid
                     container
                     spacing={5}
+                    scroll="paper"
                     direction="row"
                     justify="center"
                     alignItems="center"
-                    style={{padding:"40px"}}
+                    style={{ padding: "40px" }}
                 >
                     {projects.data.map(elem => (
                         <Grid item xs={12} sm={6} md={3} key={elem.id}>
@@ -45,30 +72,66 @@ class Project extends Component {
                                 </a>
                             </Row>
                             <Row>
-                                <Card className={"project_card_style_" + elem.id + " project_card_style"}>
+                                <Card
+                                    className={"project_card_style_" + elem.id + " project_card_style"}
+                                    onClick={() => { handleClickOpen(elem.id); }}>
                                     <CardHeader
                                         className="card_title"
                                         title={elem.name}
                                     />
                                     <div >
-                                        <img 
-                                        className="coverpic_style" 
-                                        height="150px" 
-                                        src={elem.image} />
+                                        <img
+                                            className="coverpic_style"
+                                            height="150px"
+                                            src={elem.image} />
                                     </div>
                                     <CardContent>
                                         {elem.description}
-                                        <div className="view-project-style" key={elem.id}>
+                                        <div
+                                            className="view-project-style"
+                                            key={elem.id}>
                                             View
-                                            </div>
+                                        </div>
+
                                     </CardContent>
                                 </Card>
+                                {modalId === elem.id ?
+                                    (
+                                        <Dialog
+                                            fullScreen={fullScreen}
+                                            open={open}
+                                            onClose={handleClose}
+                                            aria-labelledby="responsive-dialog-title"
+                                        >
+                                            <DialogTitle id="responsive-dialog-title">{elem.name}</DialogTitle>
+                                            <DialogContent>
+                                                <div style={{ borderStyle: "solid", borderColor: "black" }}>
+                                                    <img height="290px" src={elem.modalImage}></img>
+                                                </div>
+                                                <DialogContentText>
+                                                    <div
+                                                        className="mt-2"
+                                                        dangerouslySetInnerHTML={{ __html: elem.dialogData }}>
+                                                    </div>
+                                                </DialogContentText>
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <a
+                                                    target="_blank"
+                                                    href={elem.github}
+                                                    className="github_style_modal">
+                                                    <img height="40px" src={elem.svg}></img>
+                                                </a>
+                                                <Button onClick={handleClose} color="primary" autoFocus>
+                                                    Done
+                                                </Button>
+                                            </DialogActions>
+                                        </Dialog>) : ""}
                             </Row>
                         </Grid>
                     ))}
                 </Grid></div> </div>
-        </div>);
-    }
+    </div>);
 }
 
-export default withStyles(useStyles)(Project);
+// export default withStyles(useStyles)(Project);
